a4ab1a940852cbf371e7715859bcab835b1d6319
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-02-09 23:21:30 +0530

    changes

---
+++ p1.py
@@ -0,0 +1,17 @@
+
+
+list1=[1,2,3,4,5,6]
+list2=[]
+list3=[]
+int ch
+
+while (ch==0):
+ print("Enter your choice :")
+ print("1.push")
+ print("2.pop")
+ print("0.exit")
+input(ch)
+
+print(list)
+
+
