0167240dd8096cfadc65a0af8573faf671cadd98
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-05-04 23:07:56 +0530

    fact

--- Factorial.py
+++ Factorial.py
@@ -1,10 +1,11 @@
 
 def cal_fact(num):
-    fact = 1
-    for i in range(0,num+1):
+    fact=1
+    for i in range(1,num+1):
         fact *=i
-    print("the factorail is :")
-    print(fact)
-cal_fact(5)
+    print("Factorial is : ",fact)
+number=int(input("Enter number for Factorial : "))
+cal_fact(number)
+
 
 
