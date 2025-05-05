320c5eaf64961e485123b87becff6ca2aaa71a82
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-05-05 13:28:29 +0530

    List

--- p1.py
+++ p1.py
@@ -1,17 +1,12 @@
+l1=[]
+print("enter no of elements : ")
+length=int(input())
 
+for i in range(length):
+ num=int(input("enetr elements : "))
+ l1.append(num)
 
-list1=[1,2,3,4,5,6]
-list2=[]
-list3=[]
-int ch
+print(l1)
 
-while (ch==0):
- print("Enter your choice :")
- print("1.push")
- print("2.pop")
- print("0.exit")
-input(ch)
-
-print(list)
 
 
